c53cd6f7126d8a84adda383bf31cbd8a9ff1c003
JahanRahman <53070736+JahanRahman@users.noreply.github.com>
2020-06-07 12:36:08 -0400

    Add files via upload

--- app.R
+++ app.R
@@ -13,57 +13,59 @@ load("processed.RData")
 
 ui <-{
   fluidPage(
+  title = "Grow to Give.",
   setBackgroundImage(src="background.png"),
   tags$head(includeHTML("ad-sense.html")), #google adsense
   withAnim(),
   theme = "shiny.css",
   tags$script(includeHTML("propeller.html")),
-  navbarPage(id = "navbarpage", title = tags$h4(style = "font-family:Andale Mono;font-size: 25px"),
+  navbarPage(id = "navbarpage", title = tags$h4(style = "font-family:Raleway-Regular, Fallback, sans-serif;font-size: 25px"),
     tabPanel(
-      tags$div(id="hometab", tags$h4(style = "font-family:Andale Mono; font-size: 25px; color: #000000", "Home")),
+      tags$div(id="hometab", tags$h4(style = "font-family:Raleway-Regular, Fallback, sans-serif; font-size: 25px; color: #000000", "Home")),
       br(),
       br(),
       br(),
       br(),
       br(),
-             fluidRow(column(width = 12, tags$div(id = "title", tags$p(style = "font-family:Andale Mono;font-size: 140px;color: #000000; text-align: center", "Grow to Give.")))),
-             fluidRow(column(width = 12,tags$div(id = "subtitle", tags$p(style = "font-family:Andale Mono;font-size: 17px; color: #000000; text-align: center", "All ad revenue from this site goes to initiatives combatting racial injustice.",  tags$b("You learn, you contribute."))))),
-             fluidRow(column(width = 3, offset = 5,tags$div(id = 'videobutton', actionButton("shown", tags$b(style="font-family:Andale Mono; font-size: 20px;", "contribute NOW"), style="color: #000000; background:rgb(0,0,0,0); border-color: #000000")))),
+             fluidRow(column(width = 12, tags$div(id = "title", tags$p(style = "font-family:Raleway-Regular, Fallback, sans-serif;font-size: 140px;color: #000000; text-align: center", "Grow to Give.")))),
+             fluidRow(column(width = 12,tags$div(id = "subtitle", tags$p(style = "font-family:Raleway-Regular, Fallback, sans-serif;font-size: 17px; color: #000000; text-align: center", "All ad revenue from this site goes to initiatives combatting racial injustice.",  tags$b("You learn, you contribute."))))),
+             fluidRow(column(width = 3, offset= 5, tags$div(id = 'videobutton', actionButton("shown", tags$b(style="font-family:Raleway-Regular, Fallback, sans-serif; font-size: 20px;", "contribute NOW"), style="color: #000000; background:rgb(0,0,0,0); border-color: #000000")))),
 
       ),
-    tabPanel(tags$div(id = "learnmenu",tags$h4(style = "font-family:Andale Mono;font-size: 25px; color: #000000", "Learn")),
+    tabPanel(tags$div(id = "learnmenu",tags$h4(style = "font-family:Raleway-Regular, Fallback, sans-serif;font-size: 25px; color: #000000", "Learn")),
              verbatimTextOutput("url"),
-             fluidRow(column(width = 1, offset= 10, actionButton("confirm", tags$div(id = "confirmselection", style = "font-family: Andale Mono; font-size: 18px", tags$b("View Selection")), style="color: #000000; background:rgb(0,0,0,0); border-color: #000000"))),
-             DT::dataTableOutput("learningresources"),
+             fluidRow(column(width = 1, offset= 10, actionButton("confirm", tags$div(id = "confirmselection", style = "font-family: Raleway-Regular, Fallback, sans-serif; font-size: 18px", tags$b("View Selection")), style="color: #000000; background:rgb(0,0,0,0); border-color: #000000"))),
+             tags$br(),
+             tags$div(style = "background-color: rgb(210,210,0, 1)",DT::dataTableOutput("learningresources")),
              ),
-    navbarMenu(tags$div(id="donatemenu", tags$h4(style = "font-family:Andale Mono;font-size: 25px; color: #000000", "Donate")),
-               
+    tabPanel(tags$div(id = "sharetab", tags$h4(style = "font-family:Raleway-Regular, Fallback, sans-serif;font-size: 25px; color: #000000", "Share"))),
+    tabPanel(tags$div(id="visualizetab", tags$h4(style = "font-family:Raleway-Regular, Fallback, sans-serif;font-size: 25px; color: #000000", "Visualize"))
+             
+    ),
+    tabPanel(tags$div(id="donatemenu", tags$h4(style = "font-family:Raleway-Regular, Fallback, sans-serif;font-size: 25px; color: #000000", "Donate"))
+             
+             
+             ),
+    
+    
+    navbarMenu(tags$div(id="moremenu", tags$h4(style = "font-family:Raleway-Regular, Fallback, sans-serif;font-size: 25px; color: #000000", "More")),
                "----",
-               tabPanel(tags$h4(style = "font-family:Andale Mono;font-size: 18px; color: #000000", "Organizations/Initiatives"),
-                        tags$i(tags$h4(style = "font-family:Andale Mono;font-size: 18px; color: #FFFF00", "The names change but the color doesn't"))),
+               tabPanel(tags$h4(style = "font-family:Raleway-Regular, Fallback, sans-serif;font-size: 18px; color: #000000", "Sharing")),
                "----",
-               tabPanel(tags$h4(style = "font-family:Andale Mono;font-size: 18px; color: #000000", "Bail Funds")),
+               tabPanel(tags$h4(style = "font-family:Raleway-Regular, Fallback, sans-serif;font-size: 18px; color: #000000", "Additional Resources")),
                "----",
-               tabPanel(tags$h4(style = "font-family:Andale Mono;font-size: 18px; color: #000000", "Rebuilding Funds")),
-               "----"),
-    navbarMenu(tags$div(id="moremenu", tags$h4(style = "font-family:Andale Mono;font-size: 25px; color: #000000", "More")),
-               "----",
-               tabPanel(tags$h4(style = "font-family:Andale Mono;font-size: 18px; color: #000000", "Additional Resources")),
-               "----",
-               tabPanel(tags$h4(style = "font-family:Andale Mono;font-size: 18px; color: #000000", "Feedback")),
-               "----",
-               tabPanel(tags$h4(style = "font-family:Andale Mono;font-size: 18px; color: #000000", "Proof of Donation"),
-                        tags$h4(style = "font-family:Andale Mono;font-size: 18px; color: #000000", "Proof of Donation will be posted here when enough ad revenue is generated to reach payout minimum.")
+               tabPanel(tags$h4(style = "font-family:Raleway-Regular, Fallback, sans-serif;font-size: 18px; color: #000000", "Proof of Donation"),
+                        tags$h4(style = "font-family:Raleway-Regular, Fallback, sans-serif;font-size: 18px; color: #000000", "Proof of Donation will be posted here when enough ad revenue is generated to reach payout minimum.")
                         ),
                "----")),
   bsModal("modalExample", 
-          title = tags$h3(style = "font-family:Andale Mono", "All ad revenue from this video goes towards supporting the causes listed. Hit close to play in the background. View on YouTube directly to maximize contribution. (source: Zoe Amira)"), 
+          title = tags$h3(style = "font-family:Raleway-Regular, Fallback, sans-serif", "All ad revenue from this video goes towards supporting the causes listed. Hit close to play in the background. View on YouTube directly to maximize contribution. (source: Zoe Amira)"), 
           "shown", 
-          size = "large",
-          fluidRow(column(width = 5, offset = 2, tags$div(id= "videoelement", htmlOutput('vidframe'))))
+          size = "Large",
+          fluidRow(column(width = 5, tags$div(id= "videoelement", htmlOutput('vidframe'))))
           ),
   bsModal("selectoutput", 
-          title = tags$h3(style = "font-family:Andale Mono", "Selected Entry"), 
+          title = tags$h3(style = "font-family:Raleway-Regular, Fallback, sans-serif", "Selected Entry"), 
           'confirm',
           size = "large",
           fluidRow(column(width = 5, offset = 2, htmlOutput('selectedresource')))
@@ -76,11 +78,11 @@ ui <-{
   
 server<- function(input, output, session){
   output$vidframe <- renderUI({
-    test<-HTML('<iframe width="560" height="315" src="https://www.youtube.com/embed/bCgLa25fDHM?autoplay=1" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>')
+    test<-HTML('<iframe width="400" height="315" src="https://www.youtube.com/embed/bCgLa25fDHM?autoplay=1" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>')
     test
   })
   
-  
+
   output$learningresources <-DT::renderDataTable({read_in}, selection="single", options = list(dom = 'tp', lengthMenu = c(20, 50, 100), pageLength = 50))
 
   clicked <- eventReactive(input$learningresources_rows_selected,{
@@ -103,14 +105,20 @@ server<- function(input, output, session){
   animatebutton <-startAnim(session, id = 'videobutton', "fadeInUp")
   
   animatehome <-startAnim(session, id = 'hometab', "fadeInDown")
+  animateshare <-startAnim(session, id = 'sharetab', "fadeInDown")
   animatelearn <-startAnim(session, id = 'learnmenu', "fadeInDown")
+  animatevisualize<-startAnim(session, id = 'visualizetab', "fadeInDown")
   animatedonate <-startAnim(session, id = 'donatemenu', "fadeInDown")
   animatemore <-startAnim(session, id = 'moremenu', "fadeInDown")
   
   hoveranimatehome <- observe(addHoverAnim(session, 'hometab', 'pulse'))
+  hoveranimateshare <- observe(addHoverAnim(session, 'share', 'pulse'))
   hoveranimatelearn <- observe(addHoverAnim(session, 'learnmenu', 'pulse'))
+  hoveranimatevisualize <- observe(addHoverAnim(session, 'visualizetab', 'pulse'))
   hoveranimatedonate <- observe(addHoverAnim(session, 'donatemenu', 'pulse'))
   hoveranimatemore <- observe(addHoverAnim(session, 'moremenu', 'pulse'))
+  
+  hoveranimatetitle <- observe(addHoverAnim(session, 'title', 'pulse'))
   
   hoveranimatebutton <- observe(addHoverAnim(session, 'videobutton', 'pulse'))
   hoveranimatebutton2 <- observe(addHoverAnim(session, 'confirmselection', 'pulse'))
